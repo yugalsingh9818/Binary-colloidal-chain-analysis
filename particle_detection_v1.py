e3bd60475a06b97dd89f11a97082243ad1de0b0f
yugalsingh9818 <yugalsingh9818@gmail.com>
2026-06-17 15:10:03 +0530

    Create particle_detection_v1.py

---
+++ particle_detection_v1.py
@@ -0,0 +1,23 @@
+"""
+PROJECT: Binary Chain Analyzer
+
+VERSION: V1
+
+GOAL:
+1. Load microscope image
+2. Convert image to grayscale
+3. Apply thresholding
+4. Visualize particles/chains
+5. Perform initial contour detection
+
+INPUT:
+Microscope image (.tif)
+
+OUTPUT:
+Threshold image
+Detected objects
+
+AUTHOR:
+Yugal Singh
+PhD Scholar, IIT Goa
+"""
